32493c87710dbee49f07b625347cfb9d56c8f3b5
Niklas Klein <mail@taig.io>
2021-09-21 00:48:53 +0200

    Use munit fixtures

--- modules/core/src/test/scala/io/taig/pygments/PygmentsTest.scala
+++ modules/core/src/test/scala/io/taig/pygments/PygmentsTest.scala
@@ -1,221 +1,167 @@
 package io.taig.pygments
 
-import cats.effect.{IO, Resource}
+import cats.effect.IO
 import cats.syntax.all._
 import munit.CatsEffectSuite
 
 import scala.io.Source
 
 abstract class PygmentsTest extends CatsEffectSuite {
-  def pygments: Resource[IO, Pygments[IO]]
+  def pygments: Fixture[Pygments[IO]]
+
+  override def munitFixtures: Seq[Fixture[_]] = List(pygments)
 
   test("Brainfuck") {
-    pygments
-      .use(_.tokenize("Brainfuck", HelloWord.Brainfuck))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Brainfuck", HelloWord.Brainfuck).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("C") {
-    pygments
-      .use(_.tokenize("C", HelloWord.C))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("C", HelloWord.C).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Cobol") {
-    pygments
-      .use(_.tokenize("Cobol", HelloWord.Cobol))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Cobol", HelloWord.Cobol).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("CoffeeScript") {
-    pygments
-      .use(_.tokenize("CoffeeScript", HelloWord.CoffeeSCript))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("CoffeeScript", HelloWord.CoffeeSCript).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Crystal") {
-    pygments
-      .use(_.tokenize("Crystal", HelloWord.Crystal))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Crystal", HelloWord.Crystal).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("CSS") {
-    pygments
-      .use(_.tokenize("Css", HelloWord.Css))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Css", HelloWord.Css).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("C++") {
-    pygments
-      .use(_.tokenize("Cpp", HelloWord.CPlusPlus))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Cpp", HelloWord.CPlusPlus).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("C#") {
-    pygments
-      .use(_.tokenize("CSharp", HelloWord.CSharp))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("CSharp", HelloWord.CSharp).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Dart") {
-    pygments
-      .use(_.tokenize("Dart", HelloWord.Dart))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Dart", HelloWord.Dart).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Elm") {
-    pygments
-      .use(_.tokenize("Elm", HelloWord.Elm))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Elm", HelloWord.Elm).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Fortran") {
-    pygments
-      .use(_.tokenize("Fortran", HelloWord.Fortran))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Fortran", HelloWord.Fortran).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("F#") {
-    pygments
-      .use(_.tokenize("FSharp", HelloWord.FSharp))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("FSharp", HelloWord.FSharp).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Go") {
-    pygments
-      .use(_.tokenize("Go", HelloWord.Go))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Go", HelloWord.Go).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Groovy") {
-    pygments
-      .use(_.tokenize("Groovy", HelloWord.Groovy))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Groovy", HelloWord.Groovy).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Haskell") {
-    pygments
-      .use(_.tokenize("Haskell", HelloWord.Haskell))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Haskell", HelloWord.Haskell).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("HTML") {
-    pygments
-      .use(_.tokenize("Html", HelloWord.Html))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Html", HelloWord.Html).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Java") {
-    pygments
-      .use(_.tokenize("Java", HelloWord.Java))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Java", HelloWord.Java).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("JavaScript") {
-    pygments
-      .use(_.tokenize("Javascript", HelloWord.JavaScript))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Javascript", HelloWord.JavaScript).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Julia") {
-    pygments
-      .use(_.tokenize("Julia", HelloWord.Julia))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Julia", HelloWord.Julia).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Lua") {
-    pygments
-      .use(_.tokenize("Lua", HelloWord.Lua))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Lua", HelloWord.Lua).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("MATLAB") {
-    pygments
-      .use(_.tokenize("Matlab", HelloWord.Matlab))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Matlab", HelloWord.Matlab).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("PHP") {
-    pygments
-      .use(_.tokenize("Php", HelloWord.Php))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Php", HelloWord.Php).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Python") {
-    pygments
-      .use(_.tokenize("Python", HelloWord.Python))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Python", HelloWord.Python).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("R") {
-    pygments
-      .use(_.tokenize("S", HelloWord.R))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("S", HelloWord.R).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Ruby") {
-    pygments
-      .use(_.tokenize("Ruby", HelloWord.Ruby))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Ruby", HelloWord.Ruby).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Scala") {
-    pygments
-      .use(_.tokenize("Scala", HelloWord.Scala))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Scala", HelloWord.Scala).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("SQL") {
-    pygments
-      .use(_.tokenize("Sql", HelloWord.Sql))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Sql", HelloWord.Sql).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Swift") {
-    pygments
-      .use(_.tokenize("Swift", HelloWord.Swift))
-      .map(obtained => assert(obtained.nonEmpty))
+    pygments().tokenize("Swift", HelloWord.Swift).map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Build.hs") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/Build.hs")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Haskell", code)))
+      .flatMap(code => pygments().tokenize("Haskell", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
   test("core.rb") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/core.rb")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Ruby", code)))
+      .flatMap(code => pygments().tokenize("Ruby", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Expr.java") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/Expr.java")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Java", code)))
+      .flatMap(code => pygments().tokenize("Java", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
   test("module.py") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/module.py")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Python", code)))
+      .flatMap(code => pygments().tokenize("Python", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
   test("MonadCancel.scala") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/MonadCancel.scala")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Scala", code)))
+      .flatMap(code => pygments().tokenize("Scala", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
   test("ReactDOM.js") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/ReactDOM.js")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Javascript", code)))
+      .flatMap(code => pygments().tokenize("Javascript", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
   test("Resource.scala") {
     IO.blocking(Source.fromInputStream(getClass.getResourceAsStream("/Resource.scala")).mkString)
-      .flatMap(code => pygments.use(_.tokenize("Scala", code)))
+      .flatMap(code => pygments().tokenize("Scala", code))
       .map(obtained => assert(obtained.nonEmpty))
   }
 
@@ -225,15 +171,13 @@ abstract class PygmentsTest extends CatsEffectSuite {
     val scala = List.fill(100)(("Scala", HelloWord.Scala))
     val all = java ++ javascript ++ scala
 
-    pygments.use { linguist =>
-      for {
-        java <- linguist.tokenize("Java", HelloWord.Java)
-        javascript <- linguist.tokenize("Javascript", HelloWord.JavaScript)
-        scala <- linguist.tokenize("Scala", HelloWord.Scala)
-        obtained <- all.parTraverse { case (language, code) => linguist.tokenize(language, code) }
-      } yield {
-        assertEquals(obtained, expected = List.fill(100)(java) ++ List.fill(100)(javascript) ++ List.fill(100)(scala))
-      }
+    for {
+      java <- pygments().tokenize("Java", HelloWord.Java)
+      javascript <- pygments().tokenize("Javascript", HelloWord.JavaScript)
+      scala <- pygments().tokenize("Scala", HelloWord.Scala)
+      obtained <- all.parTraverse { case (language, code) => pygments().tokenize(language, code) }
+    } yield {
+      assertEquals(obtained, expected = List.fill(100)(java) ++ List.fill(100)(javascript) ++ List.fill(100)(scala))
     }
   }
 }
--- modules/graalvm-python/src/it/scala/io/taig/pygments/GraalVmPythonPygmentsIntegrationTest.scala
+++ modules/graalvm-python/src/it/scala/io/taig/pygments/GraalVmPythonPygmentsIntegrationTest.scala
@@ -4,40 +4,56 @@ import cats.effect.{IO, Resource}
 
 import java.nio.file.{Path, Paths}
 
+abstract class GraalVmPythonPygmentsIntegrationTest extends PygmentsTest {
+  def resource: Resource[IO, Pygments[IO]]
+
+  override val pygments: Fixture[Pygments[IO]] = new Fixture[Pygments[IO]]("pygments") {
+    var instance: Pygments[IO] = _
+
+    var release: IO[Unit] = IO.unit
+
+    override def apply(): Pygments[IO] = instance
+
+    override def beforeAll(): Unit = {
+      val (instance, release) = resource.allocated.unsafeRunSync()
+      this.instance = instance
+      this.release = release
+    }
+
+    override def afterAll(): Unit = release.unsafeRunSync()
+  }
+}
+
 object GraalVmPythonPygmentsIntegrationTest {
   val Executable: Path = Paths.get(System.getenv("JAVA_HOME") + "/languages/python/scala-pygments/bin/python")
 }
 
-final class DefaultGraalVmPythonPygmentsIntegrationTest extends PygmentsTest {
-  override val pygments: Resource[IO, Pygments[IO]] =
+final class DefaultGraalVmPythonPygmentsIntegrationTest extends GraalVmPythonPygmentsIntegrationTest {
+  override val resource: Resource[IO, Pygments[IO]] =
     GraalVmPythonPygments.default[IO](GraalVmPythonPygmentsIntegrationTest.Executable)
 
   test("don't wrap values in apostrophes") {
-    pygments.use { pygments =>
-      assertIO(
-        obtained = pygments.tokenize("Java", "foobar").map(_.headOption.map(_.code)),
-        returns = Some("foobar")
-      )
-    }
+    assertIO(
+      obtained = pygments().tokenize("Java", "foobar").map(_.headOption.map(_.code)),
+      returns = Some("foobar")
+    )
   }
 
   test("don't add a trailing linebreak") {
-    pygments.use { pygments =>
-      for {
-        withLinebreak <- pygments.tokenize("Java", "foobar\n")
-        withoutLinebreak <- pygments.tokenize("Java", "foobar")
-      } yield {
-        assertEquals(
-          obtained = withLinebreak,
-          expected = List(Fragment(Token.Name(None), "foobar"), Fragment(Token.Text(None), "\\n"))
-        )
-        assertEquals(obtained = withoutLinebreak, expected = List(Fragment(Token.Name(None), "foobar")))
-      }
+    for {
+      withLinebreak <- pygments().tokenize("Java", "foobar\n")
+      withoutLinebreak <- pygments().tokenize("Java", "foobar")
+    } yield {
+      assertEquals(
+        obtained = withLinebreak,
+        expected = List(Fragment(Token.Name(None), "foobar"), Fragment(Token.Text(None), "\\n"))
+      )
+      assertEquals(obtained = withoutLinebreak, expected = List(Fragment(Token.Name(None), "foobar")))
     }
   }
 }
 
-final class PooledGraalVmPythonPygmentsIntegrationTest extends PygmentsTest {
-  override val pygments: Resource[IO, Pygments[IO]] =
+final class PooledGraalVmPythonPygmentsIntegrationTest extends GraalVmPythonPygmentsIntegrationTest {
+  override val resource: Resource[IO, Pygments[IO]] =
     GraalVmPythonPygments.pooled[IO](GraalVmPythonPygmentsIntegrationTest.Executable, size = 2)
 }
